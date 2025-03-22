b634e849072d420b3766e14e7f1f4c83bf57a4bb
strdoffin <undefu5624@gmail.com>
2025-03-22 13:08:06 +0700

    add usercomment3

--- frontend/src/components/compage3/usercomment3.tsx
+++ frontend/src/components/compage3/usercomment3.tsx
@@ -1,7 +1,20 @@
+import { MoveRight, MoveLeft } from "lucide-react";
 export default function Usercomment3() {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div>
+            <div className="w-full flex items-center justify-center px-60 py-32">
+                <div className="w-full flex justify-between">
+                    <p className="text-4xl font-bold">Testimonials</p>
+                    <div>
+                        <button className="p-2 px-10 rounded-lg bg-gray-100">
+                            <MoveLeft size={40} /> 
+                        </button>
+                        <button className="p-2 px-10 rounded-lg bg-gray-100">
+                            <MoveRight size={40} /> 
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
